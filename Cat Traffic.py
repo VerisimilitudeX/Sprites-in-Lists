400739767f03629e08e8107dec3b60156fd91970
Verisimilitude <acharyapiyush1@outlook.com>
2022-05-09 20:57:30 -0700

    Update Cat Traffic.py

--- Cat Traffic.py
+++ Cat Traffic.py
@@ -1,8 +1,3 @@
-"""
-LESSON: 5.4 - Sprites in Lists
-EXERCISE: Cat Traffic
-"""
-
 #### ---- SET UP ---- ####
 
 # --- Libraries --- #
@@ -10,7 +5,6 @@ import tsk
 import pygame
 import random
 pygame.init()
-
 
 # --- Display Variables --- #
 window = pygame.display.set_mode([1018, 573])
@@ -20,8 +14,6 @@ floor = tsk.Sprite("CatRoom.jpg", 0, 0)
 v_cat_speed = 0.06
 h_cat_speed = 0.1
 timer = 0
-
-
 
 # --- Create cats --- #
 
@@ -33,14 +25,9 @@ cats.append(kitten)
 
 
 #### ---- RE-STRUCTURE 1 ---- ####
-
-# Create these three sprites with a for-each loop.
-# Add the sprites to the two empty lists above.
-
 cat1 = tsk.Sprite("GentleCat.png", 100, 50)
 cat2 = tsk.Sprite("GentleCat.png", 400, 50)
 cat3 = tsk.Sprite("GentleCat.png", 700, 50)
-
 
 #### ---- MAIN LOOP ---- ####
 drawing = True
@@ -51,15 +38,11 @@ while drawing:
         if event.type == pygame.QUIT:
             drawing = False
 
-
     #### --- MOVE CATS --- ####
 
     # --- Gentlemen Cats --- #
 
     #### ---- RE-STRUCTURE 2 ---- ####
-
-    # Use a for-each loop to shorten this section.
-
     at_edge = False
 
     if not pygame.sprite.collide_rect(cat1, kitten):
@@ -93,7 +76,6 @@ while drawing:
         v_cat_speed *= -1
 
     # --- Kitten --- #
-
     direction = 1
     if h_cat_speed < 0:
         direction = -1
@@ -137,25 +119,16 @@ while drawing:
 
     cats = cats_by_height
 
-
     # --- Draw --- #
     floor.draw()
 
     #### ---- RE-STRUCTURE 3 ---- ####
-
-    # Draw the cats
-
     cat1.draw()
     cat2.draw()
     cat3.draw()
     kitten.draw()
 
-    # FLIP the display
     pygame.display.flip()
 
-    # TICK the clock at 30 FPS
     c.tick(30)
     timer += c.get_time()
-
-
-# Turn in your Coding Exercise.
