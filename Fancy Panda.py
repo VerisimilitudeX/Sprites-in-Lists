1e2206dfd685d352f353b942e0c8c250ed968a29
Verisimilitude <acharyapiyush1@outlook.com>
2022-05-09 20:58:37 -0700

    Update Fancy Panda.py

--- Fancy Panda.py
+++ Fancy Panda.py
@@ -1,8 +1,3 @@
-"""
-LESSON: 5.4 - Sprites in Lists
-TECHNIQUE 4: Destroy Sprite
-PRACTICE 2
-"""
 import pygame
 import tsk
 import random
@@ -20,19 +15,16 @@ panda_timer = 0
 
 drawing = True
 while drawing:
-    # Event Loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             drawing = False
 
-    # New pandas
     panda_timer += c.get_time()
     if panda_timer >= panda_spawn_time:
         panda_timer = 0
         panda = tsk.Sprite("Panda.png", -400, 150)
         pandas.append(panda)
 
-    # Create a fancy panda
     pandas_to_remove = []
 
     for panda in pandas:
@@ -40,19 +32,14 @@ while drawing:
             fancy_panda = tsk.Sprite("PandaGentleman.png", panda.x, panda.y + 10)
             pandas.append(fancy_panda)
 
-            # Mark the current panda for removal
             pandas_to_remove.append(panda)
 
-    # Remove selected pandas
     for panda in pandas_to_remove:
         pandas.remove(panda)
 
-
-    # Move pandas
     for panda in pandas:
         panda.center_x += 0.15 * c.get_time()
 
-    # Draw
     background.draw()
     for panda in pandas:
         panda.draw()
@@ -60,6 +47,3 @@ while drawing:
 
     c.tick(30)
     pygame.display.flip()
-
-
-# Turn in your Coding Exercise.
